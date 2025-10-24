2cf1349dfa66d10338a0e7e27a8efdf4050eb27a
Source <78887483+SourcePhobia@users.noreply.github.com>
2025-10-24 02:18:55 -0400

    Update loader.bat

--- loader.bat
+++ loader.bat
@@ -1,11 +1,12 @@
 @echo off
-REM Check Python
+REM
 python --version >nul 2>&1
 IF ERRORLEVEL 1 (
-    REM Use PowerShell to download & install Python silently
+    REM
     powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe' -OutFile '$env:TEMP\python-installer.exe'; Start-Process -FilePath '$env:TEMP\python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait; Remove-Item '$env:TEMP\python-installer.exe'"
 )
 
-REM Run bootstrapper
+REM
 python bootstrapper.py
 pause
+
