bbf7a03e02bc9ed6069146e4328e0ab39f620cb4
Source <78887483+SourcePhobia@users.noreply.github.com>
2025-10-24 02:25:56 -0400

    Update loader.bat

--- loader.bat
+++ loader.bat
@@ -1,12 +1,31 @@
 @echo off
-REM
+setlocal
+
 python --version >nul 2>&1
 IF ERRORLEVEL 1 (
-    REM
-    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe' -OutFile '$env:TEMP\python-installer.exe'; Start-Process -FilePath '$env:TEMP\python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait; Remove-Item '$env:TEMP\python-installer.exe'"
+    echo [Launcher] Python not found.
+
+    choice /M "Do you want to download and install Python now?"
+    IF ERRORLEVEL 2 (
+        echo [Launcher] Installation canceled by user.
+        pause
+        exit /B
+    )
+
+    echo [Launcher] Downloading & installing Python...
+
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe' -OutFile '$env:TEMP\python-installer.exe'"
+
+    powershell -Command "Start-Process -FilePath '$env:TEMP\python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait"
+
+    powershell -Command "Remove-Item '$env:TEMP\python-installer.exe'"
+
+    echo [Launcher] Python installed.
+) ELSE (
+    echo [Launcher] Python detected.
 )
 
-REM
+echo [Launcher] Running bootstrapper...
 python bootstrapper.py
-pause
 
+pause
